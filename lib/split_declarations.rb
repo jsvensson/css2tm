711840e06864e837307960fa571f9d63bd429536
Johan Svensson <johan@atomicplayboy.net>
2012-09-29 18:32:21 +0200

    Turn key into symbol

--- lib/split_declarations.rb
+++ lib/split_declarations.rb
@@ -1,10 +1,11 @@
 module SplitDeclarations
+	# Build hash list out of CSS declarations
 	def self.split(dec)
 		dec = dec.split(/; ?/)
 		list = {}
 		dec.each do |d|
 			pair = d.split(/: ?/)
-			list[pair[0]] = pair[1]
+			list[pair[0].to_sym] = pair[1]
 		end
 		list
 	end
