27df745c133bc27b606dbab004f363cdd8010982
Johan Svensson <johan@atomicplayboy.net>
2012-09-29 18:33:12 +0200

    As good as done.

--- css2tm.rb
+++ css2tm.rb
@@ -6,15 +6,38 @@ Dir["./lib/*.rb"].each { |f| require f }
 
 parser = CssParser::Parser.new
 
-parser.load_file!('tests/test.css')
+parser.load_file!('tests/kestrel.css')
+
+theme = {
+	settings: []
+}
+
+# Build theme settings
+themename = SplitDeclarations.split(parser.find_by_selector('#theme').first)
+theme.merge!(themename)
+
+settings = {}
+settings[:settings] = SplitDeclarations.split(parser.find_by_selector('#settings').first)
+theme[:settings] << settings
 
 parser.each_selector do |selector, declaration, specificity|
-	puts selector
+	next if (selector == '#theme' or selector == '#settings')
 
-	hash = SplitDeclarations.split(declaration)
-	hash['scope'] = selector
-	puts hash
+	h = SplitDeclarations.split(declaration)
 
-	puts ThemeBlock.create(hash)
+	# Build the selector hash
+	selhash = {
+		name: h[:name],
+		scope: selector,
+		settings: {}
+	}
+
+	h.delete(:name)  # We don't want it inside the settings key
+	selhash[:settings] = h
+
+	theme[:settings] << selhash
+
 end
 
+#puts theme
+puts theme.to_plist
