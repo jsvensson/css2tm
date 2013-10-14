0abbfc6f1a2355b6e42c7a2d88072e9bbe156c63
Johan Svensson <johan@atomicplayboy.net>
2013-10-14 21:21:09 +0200

    Tabs to spaces

--- css2tm.rb
+++ css2tm.rb
@@ -4,13 +4,13 @@ Bundler.require :default
 
 # Build hash list out of CSS declarations
 def split_declarations(dec)
-	dec = dec.split(/; ?/)
-	list = {}
-	dec.each do |d|
-		pair = d.split(/: ?/)
-		list[pair[0].to_sym] = pair[1]
-	end
-	list
+  dec = dec.split(/; ?/)
+  list = {}
+  dec.each do |d|
+    pair = d.split(/: ?/)
+    list[pair[0].to_sym] = pair[1]
+  end
+  list
 end
 
 # Set up args
@@ -18,15 +18,15 @@ infile = ARGV[0]
 outfile = ARGV[1]
 
 if !infile then
-	puts "No input!"
-	exit
+  puts "No input!"
+  exit
 end
 
 parser = CssParser::Parser.new
 parser.load_file!(infile)
 
 theme = {
-	settings: []
+  settings: []
 }
 
 # Build theme settings
@@ -38,21 +38,21 @@ settings[:settings] = split_declarations(parser.find_by_selector('#settings').fi
 theme[:settings] << settings
 
 parser.each_selector do |selector, declaration, specificity|
-	next if (selector == '#theme' or selector == '#settings')
+  next if (selector == '#theme' or selector == '#settings')
 
-	h = split_declarations(declaration)
+  h = split_declarations(declaration)
 
-	# Build the selector hash
-	selhash = {
-		name: h[:name],
-		scope: selector,
-		settings: {}
-	}
+  # Build the selector hash
+  selhash = {
+    name: h[:name],
+    scope: selector,
+    settings: {}
+  }
 
-	h.delete(:name)  # We don't want it inside the settings key
-	selhash[:settings] = h
+  h.delete(:name)  # We don't want it inside the settings key
+  selhash[:settings] = h
 
-	theme[:settings] << selhash
+  theme[:settings] << selhash
 
 end
 
